15a197d2f1ab09256892745a440b9b5153faa064
he <he_1Xxh4@outlook.com>
2026-02-28 16:05:46 +0800

    Delete project_root directory

--- project_root/client/client.h
+++
@@ -1,8 +0,0 @@
-// client/client.h
-#ifndef CLIENT_H
-#define CLIENT_H
-
-// 发起 HTTPS 请求的函数
-int perform_https_request(const char *hostname, int port, const char *path_and_query, const char *method);
-
-#endif
